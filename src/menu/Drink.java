13f8a3d66cee44e6e99f2f50a0a93838aceaca94
Alex Gabor <>
2023-11-05 11:27:50 +0200

    committing previous changes

---
+++ src/menu/Drink.java
@@ -0,0 +1,9 @@
+package menu;
+
+
+public class Drink extends MenuItem {
+
+    public Drink(String name, double price) {
+        super("DRINK", name, price);
+    }
+}
---
+++ src/menu/Topping.java
@@ -0,0 +1,10 @@
+package menu;
+
+
+public class Topping extends MenuItem {
+
+    public Topping(String name, double price) {
+        super("TOPPING", name, price);
+    }
+}
+
